4d7334a2f2d6504ff31fd6e7589a1e34992c93ec
Jamie Brynes <jamiebrynes7@gmail.com>
2020-05-12 16:26:15 +0100

    yolo

---
+++ ci/code-coverage/upload-code-coverage.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
+if [[ -n "${DEBUG-}" ]]; then
+  set -x
+fi
+
+cd "$(dirname "$0")/../.."
+
+mkdir -p logs/coverage-results
+
+buildkite-agent artifact download \
+    logs\\coverage-results\\*.xml \
+    logs/coverage-results \
+    --step ":windows: ~ test"
+
+args=()
+args+=("-Dsonar.buildString=${BUILDKITE_MESSAGE}")
+args+=("-Dsonar.log.level=${SONAR_LOG_LEVEL:-"INFO"}")
+if [[ -n "${SONAR_PROJECT_DATE:-}" ]]; then
+  # For historical analysis. Note - can only supply a date later than the most recent one in the database.
+  args+=("-Dsonar.projectDate=${SONAR_PROJECT_DATE}")
+fi
+if [[ -n "${SONAR_REVISION:-}" ]]; then
+  # Override revision. Useful during historical analysis (i.e. override it to a tag that's being analysed).
+  args+=("-Dsonar.scm.revision=${SONAR_REVISION}")
+fi
+if [[ -n "${BUILDKITE_PULL_REQUEST:-}" && "${BUILDKITE_PULL_REQUEST}" != "false" ]]; then
+  # PR analysis, relies on BK building PRs
+  # https://docs.sonarqube.org/latest/analysis/pull-request/
+  args+=("-Dsonar.pullrequest.key=${BUILDKITE_PULL_REQUEST}")
+  args+=("-Dsonar.pullrequest.branch=${BUILDKITE_PULL_REQUEST_BASE_BRANCH}")
+  args+=("-Dsonar.pullrequest.base=develop")
+fi
+
+pushd ci/code-coverage
+  docker build --tag local/sonar-analysis .
+popd
+
+docker run \
+    --user="$(id -u):$(id -g)" \
+    -v "$(pwd):/usr/src" \
+    -it \
+    local/sonar-analysis "${args[@]}"
